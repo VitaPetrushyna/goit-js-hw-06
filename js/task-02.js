17d7a4ec7ae4c262ac664c14bac67dc8793ff7a2
VitaPetrushyna <masandra2018@ukr.net>
2022-07-22 22:50:28 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("ul#ingredients");
+console.log(listEl);
+
+ingredients.forEach(function (element) {
+  const titleEl = document.createElement("li");
+  titleEl.classList.add("item");
+  titleEl.textContent = `${element}`;
+  console.log(titleEl);
+  listEl.appendChild(titleEl);
+});
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,35 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const listImgEl = document.querySelector("ul.gallery");
+console.log(listImgEl);
+
+const imagesEl = images.map((image) => {
+  const itemImgEl = document.createElement("li");
+
+  const linkImgEl = document.createElement("img");
+
+  linkImgEl.classList.add("gallery-list");
+  linkImgEl.setAttribute("src", `${image.url}`);
+  linkImgEl.setAttribute("alt", `${image.alt}`);
+
+  itemImgEl.appendChild(linkImgEl);
+
+  console.log(itemImgEl);
+});
+
+// const totalScore = students.reduce((total, student) => {
+//   return total + student.score;
+// }, 0);
