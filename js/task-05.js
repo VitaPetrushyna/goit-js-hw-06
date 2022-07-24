65681bfdafa9606274c00781fb51372b3a5cb852
VitaPetrushyna <masandra2018@ukr.net>
2022-07-24 14:34:16 +0300

    Task 5

---
+++ js/task-05.js
@@ -0,0 +1,28 @@
+// const inputEl = document.querySelector("#name-input");
+// const outputEl = document.querySelector("#name-output");
+
+const refs = {
+  inputEl: document.querySelector("#name-input"),
+  outputEl: document.querySelector("#name-output"),
+};
+console.log(refs.outputEl);
+console.log(refs.inputEl);
+
+refs.inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
+  refs.outputEl.textContent = event.currentTarget.value || "Anonymous";
+}
+
+// refs.inputEl.addEventListener("focus", (event) => {
+//   console.log("Инпут получил фокус");
+// });
+// refs.inputEl.addEventListener("blur", (event) => {
+//   console.log("Инпут потерял фокус");
+// });
+
+// refs.inputEl.addEventListener("change", onInputChange);
+// function onInputChange(event) {
+//   console.log(event);
+// }
