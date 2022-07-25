d3559eafcab4df83f8d3ee5de5377f281db85570
VitaPetrushyna <masandra2018@ukr.net>
2022-07-25 15:35:49 +0300

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanColorEl = document.querySelector(".color");
+const btnChangeColorEl = document.querySelector(".change-color");
+
+btnChangeColorEl.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  const newColor = getRandomHexColor();
+  spanColorEl.textContent = newColor;
+  document.body.style.backgroundColor = newColor;
+}
